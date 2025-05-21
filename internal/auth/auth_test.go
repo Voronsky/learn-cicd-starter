7db8d5fce70a5011fc86ab685f20f3f753e4d09a
Voronsky <roflocopter911@gmail.com>
2025-05-20 23:24:35 -0400

    fix unit test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,7 +9,6 @@ import (
 func TestErrAuth(t *testing.T) {
 	var authHeader http.Header // Empty/zero'd value
 	_, err := GetAPIKey(authHeader)
-	t.Fatalf("Code is supposed to fail as per boot dev instruct")
 	if err == nil {
 		t.Fatalf("expected: %v, got: %v",nil,err)
 	}
