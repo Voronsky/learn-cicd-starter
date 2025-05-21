6d17c0dfc9bf5538819ccd792e882b89df43bc4a
Voronsky <roflocopter911@gmail.com>
2025-05-20 23:37:37 -0400

    whoops

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,15 +1,14 @@
 package auth
 
 import (
-	"testing"
 	"net/http"
+	"testing"
 )
-
 
 func TestErrAuth(t *testing.T) {
 	var authHeader http.Header // Empty/zero'd value
 	_, err := GetAPIKey(authHeader)
 	if err == nil {
-		t.Fatalf("expected: %v, got: %v",nil,err)
+		t.Fatalf("expected: %v, got: %v", nil, err)
 	}
 }
